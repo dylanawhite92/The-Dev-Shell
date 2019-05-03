fe1eaf18c5db23f0cc19f5d14807b64c699a898e
Dylan White <dylanawhite11@gmail.com>
2019-05-03 14:13:17 -0500

    brought in routes to server.js, receiving in postman

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ connectDB();
 // Home Endpoint
 app.get("/", (req, res) => res.send("API Running."));
 
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 // Set up server to look for environment variable when deployed,
 // but listen on port 5000 when local
 const PORT = process.env.PORT || 5000;
