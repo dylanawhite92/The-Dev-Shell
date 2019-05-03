f90765b429c86f22504bdf7d1c5456c19fcbe8ba
Dylan White <dylanawhite11@gmail.com>
2019-05-03 13:25:03 -0500

    created config folder, creating connection to cloud database

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("config");
+// Pull mongoDB connection string from default.json
+const db = config.get("mongoURI");
+
+// Async/Await connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+
+    console.log("MongoDB Connected.");
+  } catch (err) {
+    console.log(err.message);
+
+    // Exit process with failure
+    process.exit(1);
+  }
+};
