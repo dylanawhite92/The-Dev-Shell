6b841b2c7bf97d736a8682879b34e2d25d59b3fd
Dylan White <dylanawhite11@gmail.com>
2019-06-25 16:58:07 -0500

    testing posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,12 +3,15 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
 const auth = require("../../middleware/auth");
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Private
 router.post(
-  "/posts",
+  "/",
   [
     auth,
     [
@@ -22,6 +25,25 @@ router.post(
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Get name & avatar of user, as well as the user itself without sending password
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
     }
   }
 );
