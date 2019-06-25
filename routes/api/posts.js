7d31a5fe26f9589999e5bb6410ed6aa9cf661bac
Dylan White <dylanawhite11@gmail.com>
2019-06-25 16:50:39 -0500

    modifying posts.js, added error checking for posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,29 @@
 // Handle forum/messaging routing such as likes and comments
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Posts Route."));
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/posts",
+  [
+    auth,
+    [
+      check("text", "Writing something in your post is required!")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
