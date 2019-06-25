473ffeec4efa7322d10d4c325fcf33f0496578be
Dylan White <dylanawhite11@gmail.com>
2019-06-25 18:28:46 -0500

    made delete route for comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -231,6 +231,34 @@ router.post(
 // @access  Private
 router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
   try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment not found!" });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized!" });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    // Remove like from likes array
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
