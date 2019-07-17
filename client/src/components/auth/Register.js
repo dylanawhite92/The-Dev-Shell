10a5924f335cb9ad4b731b2b5f601728e7871995
Dylan White <dylanawhite11@gmail.com>
2019-07-17 16:16:43 -0500

    updating Register component with boiler, will update with states

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,48 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <section className="container">
+        <h1 className="large text-primary">Sign Up</h1>
+
+        <p className="lead">
+          <i className="fas fa-user" /> Create Your Account
+        </p>
+        <form action="dashboard.html" className="form">
+          <div className="form-group">
+            <input type="text" placeholder="Name" required />
+          </div>
+
+          <div className="form-group">
+            <input type="email" placeholder="Email Address" />
+            <small className="form-text">
+              This site uses Gravatar, so if you want a profile image, use a
+              Gravatar email!
+            </small>
+          </div>
+
+          <div className="form-group">
+            <input type="password" placeholder="Password" minlength="6" />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              minlength="6"
+            />
+          </div>
+
+          <input type="submit" value="Register" className="btn btn-primary" />
+        </form>
+
+        <p className="my-1">
+          Already have an account? <a href="login.html">Sign In</a>
+        </p>
+      </section>
+    </>
+  );
 };
 
 export default Register;
