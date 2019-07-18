abc258f5cf9ec8cb407f35cd3b6871ebf9dc2295
Dylan White <dylanawhite11@gmail.com>
2019-07-18 14:55:33 -0500

    implemented code for base login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  // Destructuring for more concise code
+  const { email, password } = formData;
+
+  // Handle form values changing
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  // Handle form submission
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log("Success.");
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h1 className="large text-primary">Sign In</h1>
+
+        <p className="lead">
+          <i className="fas fa-user" /> Sign In to Your Account
+        </p>
+        <form className="form" onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              minLength="6"
+              name="password"
+              value={password}
+              onChange={e => onChange(e)}
+            />
+          </div>
+          <input type="submit" value="Log In" className="btn btn-primary" />
+        </form>
+
+        <p className="my-1">
+          Don't have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </section>
+    </>
+  );
 };
 
 export default Login;
