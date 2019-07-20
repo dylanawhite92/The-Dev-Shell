bf08c12195ec93d02f15e3e327350a1260658210
Dylan White <dylanawhite11@gmail.com>
2019-07-20 15:42:22 -0500

    implemented remove alert action in alert.js

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,12 +2,23 @@ import uuid from "uuid";
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 
 // Able to dispatch more than one action using thunk middleware after defined parameters
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 3500) => dispatch => {
   // Get random universal ID
   const id = uuid.v4();
 
+  // Dispatch alert action when mismatched password is fired off
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  // Remove alert after 3.5 seconds
+  setTimeout(
+    () =>
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id
+      }),
+    timeout
+  );
 };
