f2fc62aa6aae7325f53a05a62235fbe2bf18f47b
Dylan White <dylanawhite11@gmail.com>
2019-07-21 15:15:30 -0500

    setup error action in auth.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { setAlert } from "./alert";
 import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
 
 // Register User
@@ -19,6 +20,14 @@ export const register = ({ name, email, password }) => async dispatch => {
       payload: res.data
     });
   } catch (err) {
-    dispatch({ type: REGISTER_FAIL });
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: REGISTER_FAIL
+    });
   }
 };
