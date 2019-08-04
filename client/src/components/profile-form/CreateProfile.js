b0cc6a278ee3cf555b54b1e6132a9613f1f4f41e
Dylan White <dylanawhite11@gmail.com>
2019-08-04 13:47:27 -0500

    able to create a profile and update mongodb with profile information

--- client/src/components/profile-form/CreateProfile.js
+++ client/src/components/profile-form/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { createProfile } from "../../actions/profile";
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   // Form Fields
   const [formData, setFormData] = useState({
     company: "",
@@ -41,6 +43,11 @@ const CreateProfile = props => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
   return (
     <>
       <h1 className="large text-primary">Create Profile</h1>
@@ -51,7 +58,7 @@ const CreateProfile = props => {
       </p>
       <small>* = Required Field</small>
 
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -218,6 +225,11 @@ const CreateProfile = props => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
+};
 
-export default CreateProfile;
+export default connect(
+  null,
+  { createProfile }
+)(withRouter(CreateProfile));
