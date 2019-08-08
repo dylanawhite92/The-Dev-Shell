ce61586f4af6c036dee3f290ce6ccd76caac6e0f
Dylan White <dylanawhite11@gmail.com>
2019-08-08 17:39:21 -0500

    created AddExperience and AddEducation components, empty as of now

---
+++ client/src/components/profile-form/AddEducation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addExperience } from "../../actions/profile";
+
+const AddEducation = props => {
+  return <div />;
+};
+
+AddEducation.propTypes = {};
+
+export default AddEducation;
