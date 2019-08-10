4be3c5592fb3e50cd3635f2bab067109bd56f73f
Dylan White <dylanawhite11@gmail.com>
2019-08-10 15:33:40 -0500

    delete account added to auth reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,7 +5,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  ACCOUNT_DELETED
 } from "../actions/types";
 
 const initialState = {
@@ -40,6 +41,7 @@ export default (state = initialState, action) => {
     case LOGIN_FAIL:
     case LOGOUT:
     case AUTH_ERROR:
+    case ACCOUNT_DELETED:
       localStorage.removeItem("token");
 
       return {
