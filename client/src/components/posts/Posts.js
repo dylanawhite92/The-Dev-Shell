23ebac66369d0ef9ce6b14b69c4590effffe4b18
Dylan White <dylanawhite11@gmail.com>
2019-08-28 18:56:05 -0500

    creating Posts.js component

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Spinner } from "../layout";
+import { getPosts } from "../../actions/post";
+
+const Posts = props => {
+  return <div></div>;
+};
+
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(Posts);
