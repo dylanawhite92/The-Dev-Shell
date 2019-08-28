c58857e1a5d39e64dd9689609903f80fc2946d8a
Dylan White <dylanawhite11@gmail.com>
2019-08-28 15:32:43 -0500

    brought in theme html to ProfileAbout.js

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -8,7 +8,34 @@ const ProfileAbout = ({
     user: { name }
   }
 }) => {
-  return <div>test</div>;
+  return (
+    <div className="profile-about bg-light p-2">
+      <h2 className="text-primary">John's Bio</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam libero
+        praesentium quidem animi, voluptates earum sit possimus quo voluptatem
+        exercitationem.
+      </p>
+
+      <div className="line"></div>
+
+      <h2 className="text-primary">Skill Set</h2>
+      <div className="skills">
+        <div className="p-1">
+          <i className="fas fa-check"></i>HTML5
+        </div>
+        <div className="p-1">
+          <i className="fas fa-check"></i>CSS3
+        </div>
+        <div className="p-1">
+          <i className="fas fa-check"></i>JavaScript
+        </div>
+        <div className="p-1">
+          <i className="fas fa-check"></i>Python
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ProfileAbout.propTypes = {
