782fa356e351f16017bdc3a5428233f519214922
Dylan White <dylanawhite11@gmail.com>
2019-08-28 17:12:19 -0500

    fixed typechecking in prop types, as well as rearranging ProfileEducation component, on which the border is not showing for some reason

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -59,6 +59,23 @@ const Profile = ({
                 <h4>No experience added</h4>
               )}
             </div>
+
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+
+              {profile.education.length > 0 ? (
+                <>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </>
+              ) : (
+                <h4>No education added</h4>
+              )}
+            </div>
           </div>
         </>
       )}
