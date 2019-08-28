043af4f35de699d27cde874e11f25c252e86257d
Dylan White <dylanawhite11@gmail.com>
2019-08-28 15:40:16 -0500

    ProfileAbout component finished, mapping through array of skills and properly rendering on profile page

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -7,37 +7,27 @@ const ProfileAbout = ({
     skills,
     user: { name }
   }
-}) => {
-  return (
-    <div className="profile-about bg-light p-2">
-      <h2 className="text-primary">John's Bio</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam libero
-        praesentium quidem animi, voluptates earum sit possimus quo voluptatem
-        exercitationem.
-      </p>
+}) => (
+  <div className="profile-about bg-light p-2">
+    {bio && (
+      <>
+        <h2 className="text-primary">{name.trim().split(" ")[0]}'s Bio</h2>
+        <p>{bio}</p>
 
-      <div className="line"></div>
+        <div className="line"></div>
+      </>
+    )}
 
-      <h2 className="text-primary">Skill Set</h2>
-      <div className="skills">
-        <div className="p-1">
-          <i className="fas fa-check"></i>HTML5
+    <h2 className="text-primary">Skill Set</h2>
+    <div className="skills">
+      {skills.map((skill, index) => (
+        <div key={index} className="p-1">
+          <i className="fas fa-check"></i> {skill}
         </div>
-        <div className="p-1">
-          <i className="fas fa-check"></i>CSS3
-        </div>
-        <div className="p-1">
-          <i className="fas fa-check"></i>JavaScript
-        </div>
-        <div className="p-1">
-          <i className="fas fa-check"></i>Python
-        </div>
-      </div>
+      ))}
     </div>
-  );
-};
-
+  </div>
+);
 ProfileAbout.propTypes = {
   profile: PropTypes.object.isRequired
 };
