731e87283806081ab8ef5c8e17a312ea8491f0b8
Dylan White <dylanawhite11@gmail.com>
2019-08-28 16:29:44 -0500

    adding to and from dates, formatted with react-moment

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -1,10 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Moment from "react-moment";
 
-const ProfileExperience = props => {
-  return <div>test</div>;
+const ProfileExperience = ({
+  experience: { company, title, location, current, to, from, description }
+}) => (
+  <div>
+    <h3 className="text-dark">{company}</h3>
+
+    <p>
+      <Moment format="YYYY/MM/DD">{from}</Moment> -{" "}
+      {!to ? "Now" : <Moment format="YYYY/MM/DD">{to}</Moment>}
+    </p>
+  </div>
+);
+
+ProfileExperience.propTypes = {
+  experience: PropTypes.array.isRequired
 };
-
-ProfileExperience.propTypes = {};
 
 export default ProfileExperience;
