1cfea70d13e14653b537095da356b59580b245b4
Dylan White <dylanawhite11@gmail.com>
2019-09-05 14:51:05 -0500

    created component for individual post

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Spinner } from "../layout";
+import { getPost } from "../../actions/post";
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost]);
+
+  return <div>post</div>;
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post);
