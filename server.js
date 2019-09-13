51f1dc48b650eac5940664023b62e1e80d92f992
Dylan White <dylanawhite11@gmail.com>
2019-09-12 19:14:21 -0500

    preparing for deployment on heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -9,14 +10,21 @@ connectDB();
 // Initialize Middleware
 app.use(express.json({ extended: false }));
 
-// Home Endpoint
-app.get("/", (req, res) => res.send("API Running."));
-
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve Static Assets in Production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Set up server to look for environment variable when deployed,
 // but listen on port 5000 when local
